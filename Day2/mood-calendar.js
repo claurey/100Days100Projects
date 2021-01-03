a07f4b099231d842dfa645127b883577cca6abc9
Claudia Reyna <reynah.claudiae@live.com>
2021-01-03 00:03:16 -0500

    challenge 2 finished

---
+++ Day2/mood-calendar.js
@@ -0,0 +1,559 @@
+let months = [
+  {
+    month: "January",
+    days: [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+  {
+    month: "February",
+    days: [
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+    ],
+  },
+  {
+    month: "March",
+    days: [
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+  {
+    month: "April",
+    days: [
+      "",
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+    ],
+  },
+  {
+    month: "May",
+    days: [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+  {
+    month: "June",
+    days: [
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+    ],
+  },
+  {
+    month: "July",
+    days: [
+      "",
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+  {
+    month: "August",
+    days: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+  {
+    month: "September",
+    days: [
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+    ],
+  },
+  {
+    month: "October",
+    days: [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+  {
+    month: "November",
+    days: [
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+    ],
+  },
+  {
+    month: "December",
+    days: [
+      "",
+      "",
+      "",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+    ],
+  },
+];
+
+const calendar = document.getElementById("calendar");
+
+const month = (onemonth = []) => {
+  let innerDays = "";
+    onemonth.forEach((months) => {
+    const calendarMonth = document.createElement("div");
+    calendarMonth.classList.add("col", "m-3");
+    calendarMonth.innerHTML = `<h4 class="text-center">${months.month}</h4>`;
+    calendar.appendChild(calendarMonth);
+
+    const tableMonth = document.createElement("table");
+    tableMonth.classList.add(
+      "table",
+      "table-borderless",
+      "vertical-align",
+      "table-sm"
+    );
+    calendarMonth.appendChild(tableMonth);
+
+    const theadTable = document.createElement("thead");
+    theadTable.innerHTML = `<tr>
+    <th class="text-center">Sun</th>
+    <th class="text-center">Mon</th>
+    <th class="text-center">Tue</th>
+    <th class="text-center">Wed</th>
+    <th class="text-center">Thu</th>
+    <th class="text-center">Fri</th>
+    <th class="text-center">Sat</th>
+    </tr>`;
+    tableMonth.appendChild(theadTable);
+
+    const tbodyTable = document.createElement("tbody");
+    tableMonth.appendChild(tbodyTable);
+
+    let j = 0;
+    for (let i = 0; i < 6; i++) {
+      const trTable = document.createElement("tr");
+      tbodyTable.appendChild(trTable);
+      for (let i = 0; i < 7; i++, j++) {
+        const tdTable = document.createElement("td");
+        tdTable.classList.add("text-center");
+        if (months.days[j] === "") {
+          trTable.appendChild(tdTable);
+          continue;
+        }
+        if (!months.days[j]) break;
+        const btnDay = document.createElement("button");
+        btnDay.classList.add("btnDay", "btn", "p-1");
+        btnDay.id = "btnByDay";
+        btnDay.innerHTML = `<div>${months.days[j]}</div>`;
+        trTable.appendChild(tdTable);
+        tdTable.appendChild(btnDay);
+      }
+    }
+  });
+};
+month(months);
+newClass = "";
+
+const btnByDay = document.querySelectorAll("#btnByDay");
+const btnSelector = document.querySelectorAll(".btnSelector");
+
+for(let i=0; i<btnByDay.length;i++){
+  btnByDay[i].onclick = (e) => {
+    btnByDay[i].classList.toggle(newClass);
+    console.log(btnByDay[i]);
+  };
+
+}
+
+
+for(let i=0; i<btnSelector.length; i++){
+  btnSelector[i].onclick = (e) => {
+
+    if (i===0)newClass="crazy";
+    if (i===1)newClass="happy";
+    if (i===2)newClass="boring";
+    if (i===3)newClass="tired";
+    if (i===4)newClass="sad";
+    if (i===5)newClass="angry";
+
+
+  };
+}
+
+
+const btnClear = document.getElementById("btnClear");
+
+btnClear.onclick = (e)=>{
+
+  for(let i=0; i<btnByDay.length;i++){
+    console.log("eliminado");
+    btnByDay[i].classList.remove("crazy","happy","boring","tired","sad","angry");
+  }
+
+}
